d00624a6669130420284feff7d0f1287eb5dc0e7
Vladyslav Kotsiuba <vladyslav.kotsiuba@gmail.com>
2023-07-02 14:03:26 +0300

    Add config vars

--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,12 @@
 import * as dotenv from "dotenv";
+import * as env from "env-var";
 
 dotenv.config({ path: ".env" });
+
+export const config = {
+  INFURA_API_KEY: env.get("INFURA_API_KEY").asString(),
+  PRIVATE_KEY: env.get("PRIVATE_KEY").asString(),
+  ETHERSCAN_API_KEY: env.get("ETHERSCAN_API_KEY").asString(),
+  GAS_REPORTER: env.get("GAS_REPORTER").default("true").asBool(),
+  COINMARKETCAP_API_KEY: env.get("COINMARKETCAP_API_KEY").asString(),
+};
